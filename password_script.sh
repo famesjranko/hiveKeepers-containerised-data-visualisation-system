6113c95e540c8523c7569504a048d6b2fa0763a8
Andrew McDonald <17961040@students.ltu.edu.au>
2022-03-15 04:33:19 +1100

    FIX: too many indentations

--- password_script.sh
+++ password_script.sh
@@ -10,14 +10,12 @@ FILE=$1
 
 while read line || [ -n "$line" ];
 do
-        # store field 1
-        NAME=$(echo $line|cut -d$FS -f1)
+  # store field 1 - username
+  NAME=$(echo $line|cut -d$FS -f1)
 
-        # store field 2
-        PASSWORD=$(echo $line|cut -d$FS -f2)
+  # store field 2 - password
+  PASSWORD=$(echo $line|cut -d$FS -f2)
 
-        # add username and encrypted password to access file
-        htpasswd -b .htpasswd $NAME $PASSWORD
+  # add username and encrypted password to access file
+  htpasswd -b .htpasswd $NAME $PASSWORD
 done < $FILE
-
-exit
